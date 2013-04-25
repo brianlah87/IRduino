c653a1d6f06192f08d975ec58649980c27b93c04
Brian Lee <cybexsoft@hotmail.com>
2013-04-25 18:14:55 +0800

    Added documentation for functions in IRduino.h

--- IRduino.h
+++ IRduino.h
@@ -20,19 +20,30 @@
 //DEFINES-------------------------------------------------------------------------
 
 //Carrier Mode
-#define CARRIERMODE_38K	38
+#define CARRIERMODE_38K	38			//represents a 38kHz carrier frequency (26uS period)
 
 //Bit Order
-#define FIRSTBIT_LSB 	0b00000001
-#define FIRSTBIT_MSB 	0b10000000
+#define FIRSTBIT_LSB 	0b00000001	//represents a Least Significant Bit First order
+#define FIRSTBIT_MSB 	0b10000000	//represents a Most Significant Bit First order
 
 //Chunk Size
-#define CHUNKSIZE_NIBBLE 4
-#define CHUNKSIZE_BYTE 8
+#define CHUNKSIZE_NIBBLE 4			//represents a Chunk Size of 4 bits
+#define CHUNKSIZE_BYTE 8			//represents a Chunk Size of 8 bits
 
 class IRduino {
 public:
-	//sequences
+
+	//	SEQUENCES
+	//
+	//	Sequences are arrays containing pulse lengths. Pulse lengths are measured in
+	//	periods of the carrier frequency.
+	//
+	//	For example, a 38kHz carrier frequency has a period of 26 microseconds, and a 0.5
+	//	millisecond pulse could be represented by roughly 19 periods.
+	//
+	//	A sequence of {10,20,30} would be transmitted as 10 periods ON, 20 periods OFF,
+	//	and 30 periods ON.
+	//
 	static uint16_t *oneSQ;
 	static uint8_t oneSQlen;
 
@@ -45,16 +56,51 @@ public:
 	static uint16_t *stopSQ;
 	static uint8_t stopSQlen;
 
-	//init
+	//	init()				Initialises the IR library.
+	//
+	//	Accepted parameters are explained above in the DEFINES section.
+	//
 	static void init(uint8_t carrierMode, uint8_t startBit, uint8_t chunkSize);
 
-	//main functions
+	//	bufferChunk()		Buffers a chunk of data.
+	//
+	//	Accepts an entire byte, but if CHUNKSIZE_NIBBLE is used, only the first
+	//	4 bytes of each chunk is transmitted. CHUNKSIZE_BYTE transmits all 8 bytes of each
+	//	chunk.
+	//
+	//	Returns the value of the chunk for checksum purposes.
+	//
 	static uint8_t bufferChunk(uint8_t chunk);
+
+	//	bufferReset()		Empties the transmit buffer.
+	//
 	static void bufferReset();
+
+	//	bufferTransmit()	Transmits chunks buffered so far.
+	//
+	//	Using FIRSTBIT_LSB  sends the Least Significant Bit first, 0b0111 sent as 1110.
+	//	Using FIRSTBIT_MSB sends the Most Significant Bit first, 0b0111 sent as 0111.
+	//
 	static void bufferTransmit();
 
-	//convenience functions
+	//	bufferZeroChunks() Buffers count number of zero filled chunks.
+	//
+	//	Some protocols pad large number of zeroes between actual data. Use this to quickly
+	//	fill those spaces when submitting chunks to the buffer.
+	//
+	//	Sends either a NIBBLE or a BYTE of zeroes, depending on CHUNKSIZE.
+	//
+	//	Returns the value 0 for checksum purposes.
+	//
 	static uint8_t bufferZeroChunks(uint8_t count);
+
+	//	bufferChunk4Bits(), bufferChunk8Bits()	buffers a chunk with the exact bits given.
+	//
+	//	Some protocols use a single bit to represent a boolean setting. Use this to quickly
+	//	set those bits and submit them as a chunk.
+	//
+	//	Returns the value of the chunk for checksum purposes.
+	//
 	static uint8_t bufferChunk4Bits(uint8_t a, uint8_t b, uint8_t c, uint8_t d);
 	static uint8_t bufferChunk8Bits(uint8_t a, uint8_t b, uint8_t c, uint8_t d,
 								 uint8_t e, uint8_t f, uint8_t g, uint8_t h);
